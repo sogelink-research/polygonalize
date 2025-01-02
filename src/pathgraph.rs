72ec611f6b8ec53b4e0bfefe9c55074af499508e
Emanuel Buttaci <emanuel.buttaci@geodan.nl>
2025-01-02 11:47:05 +0100

    Code optimization and documentation completed

--- src/pathgraph.rs
+++ src/pathgraph.rs
@@ -5,34 +5,48 @@ use super::coordinates::Coordinates;
 use super::coordinates::CoordinatesVector;
 use super::plane::PlaneMatcher;
 
+/// List of successor connections along with the plane on which they lie
+/// in accordance with their shared ancestor connection.
 type ProjectedSuccessors = Vec<(PlaneMatcher, (Coordinates, Coordinates))>;
 
+/// Successor connection projected on a specific plane.
 #[derive(Debug)]
 struct ProjectedIntersection {
+    /// Successor connection.
     successor: (Coordinates, Coordinates),
+    /// Projected angle if not collinear between ancestor and successor.
     angle: Option<f64>,
 }
 
+/// Graph builder which builds [PathGraph] as an undirected graph.
 #[derive(Debug)]
 pub struct PathGraphBuilder {
+    /// Adjacency list of neighboring coordinates connected by a line.
     adjacencies: HashMap<Coordinates, HashSet<Coordinates>>,
+    /// Threshold used to determine collinearity.
     epsilon: f64,
 }
 
+/// Graph of connections, namely oriented pairs of coordinates, built from [PathGraphBuilder].
 #[derive(Debug)]
 pub struct PathGraph {
+    /// Maps each connection to its list of projected successors.
     pub intersections: HashMap<(Coordinates, Coordinates), ProjectedSuccessors>,
+    /// Threshold used to determine collinearity.
     pub epsilon: f64,
 }
 
 impl ProjectedIntersection {
+    /// Projects `successor` on plane `plane` shared by both `current` and `successor`.
     pub fn on_plane(
         plane: &PlaneMatcher,
         current: (Coordinates, Coordinates),
         successor: (Coordinates, Coordinates),
     ) -> Self {
         ProjectedIntersection {
+            // successor
             successor,
+            // projected angle
             angle: plane.project_angle_between(
                 &CoordinatesVector::from(&current),
                 &CoordinatesVector::from(&successor),
@@ -42,17 +56,20 @@ impl ProjectedIntersection {
 }
 
 impl PathGraphBuilder {
+    /// Instantiates a graph builder from a sequence of connections, namely oriented pairs of coordinates.
     pub fn from(connections: &Vec<(Coordinates, Coordinates)>, epsilon: f64) -> Self {
+        // adjacency list of neighboring coordinates
         let mut adjacencies = HashMap::<Coordinates, HashSet<Coordinates>>::new();
-
+        // update `adjacencies` with every pair of coordinates in `connection`
         for (u, v) in connections {
+            // adds `v` to the successors of `u`
             adjacencies
                 .entry(*u)
                 .and_modify(|to| {
                     to.insert(*v);
                 })
                 .or_insert(HashSet::from([*v]));
-
+            // adds `u` to the successors of `v` because of the undirected nature of the graph
             adjacencies
                 .entry(*v)
                 .and_modify(|to| {
@@ -60,82 +77,99 @@ impl PathGraphBuilder {
                 })
                 .or_insert(HashSet::from([*u]));
         }
-
+        // finds all the coordinates that behave like a sink, namely they are dead ends
         let mut leaves = adjacencies
             .iter()
             .filter(|(_, to)| to.len() == 1)
             .map(|(leaf, _)| *leaf)
             .collect::<HashSet<_>>();
-
+        // keeps removing the coordinates from the graph that give birth to dead ends until we have none
         while !leaves.is_empty() {
+            // next generation of leaves
             let mut updated = HashSet::<Coordinates>::new();
-
+            // remove `leaf` from all its neighbors
             for leaf in &leaves {
                 if adjacencies.contains_key(leaf) {
                     if let Some(adjacent) = adjacencies[leaf].iter().next() {
+                        // if `adjacent` is now a dead end then it is added to `updated` for the next pruning round
                         if adjacencies[adjacent].len() <= 2 {
                             updated.insert(*adjacent);
                         }
-
+                        // `leaf` is removed from the neighbors of `adjacent`
                         adjacencies.entry(*adjacent).and_modify(|to| {
                             to.remove(leaf);
                         });
                     }
-
+                    // `leaf` if finally pruned from the whole adjacency list
                     adjacencies.remove(leaf);
                 }
             }
-
+            // `leaves` is now pointing to the new set of dead ends if any
             leaves = updated;
         }
-
+        // the output builder has already pruned nodes meaning coordinates
         Self {
             adjacencies,
             epsilon,
         }
     }
 
+    /// Builds an undirected graph from the pruned adjacency list of coordinates.
     pub fn build(&self) -> PathGraph {
+        // map of pairs of connections that suffer collinearity
         let mut undefined =
             HashMap::<(Coordinates, Coordinates), (Coordinates, Coordinates)>::new();
+        // map of connection and successors lying on detected planes
         let mut intersections = HashMap::<
             (Coordinates, Coordinates),
             Vec<(PlaneMatcher, Vec<ProjectedIntersection>)>,
         >::new();
-
+        // constructs the preliminar graph from the adjacency list for each `intersection` coordinates
         for (intersection, neighbors) in &self.adjacencies {
+            // iterates over neighbors of `intersection`
             for u in neighbors {
+                // `incident` connection (u, intersection) will be considered entering `intersection`
                 let incident = (*u, *intersection);
-
+                // initializes the successors list of incident if empty
                 intersections.entry(incident).or_default();
-
+                // iterates again over neighbors of `intersection`
                 for v in neighbors {
+                    // avoids adding (u, v) to the successors of (v, u) otherwise it would create an unwanted loop
                     if u != v {
+                        // constructs exiting connection (intersection, v) from coordinates `intersection`
                         let adjacent = (*intersection, *v);
+                        // constructs the plane between `incident` and its successor `adjacent`
                         let plane = PlaneMatcher::between(
                             &CoordinatesVector::from(&incident),
                             &CoordinatesVector::from(&adjacent),
                             self.epsilon,
                         );
-
+                        // initializes as an empty list the successors of `adjacent`
                         intersections.entry(adjacent).or_default();
-
+                        // delays the registration of `adjacent` is it is collinear with `incident` meaning undefined plane
                         if plane.is_undefined() {
+                            // collinear neighbors will be handled later
                             undefined.insert(incident, adjacent);
                         } else if let Some(matchers) = intersections.get_mut(&incident) {
+                            // checks whether `adjacent` lies on an already identified plane on which `incident` lies
                             let mut matched = false;
-
+                            // iterates over all previously identified planes on which `incident` lies considering its successors
                             for (matcher, successors) in matchers {
+                                // in case the plane already exists
                                 if *matcher == plane {
+                                    // match found
                                     matched = true;
+                                    // adds `adjacent` to the already existing plane as projected successor
                                     successors.push(ProjectedIntersection::on_plane(
                                         matcher, incident, adjacent,
                                     ));
                                 }
                             }
-
+                            // if `adjacent` forms a new plane with `incident` without them being collinear
                             if !matched {
+                                // the new plane is registered for `incident`
                                 intersections.entry(incident).and_modify(|matchers| {
+                                    // and `adjacent` is added as projected successor lying on that plane
                                     matchers.push((
                                         plane,
                                         vec![ProjectedIntersection::on_plane(
@@ -149,8 +183,9 @@ impl PathGraphBuilder {
                 }
             }
         }
-
+        // pairs of (`incident`, `adjacent`) being collinear are now processed
         for (incident, adjacent) in &undefined {
+            // since the plane is undefined, we add `adjacent` on any detected plane on which `incident` lies
             if let Some(matchers) = intersections.get_mut(incident) {
                 for (matcher, successors) in matchers {
                     successors.push(ProjectedIntersection::on_plane(
@@ -158,7 +193,7 @@ impl PathGraphBuilder {
                     ));
                 }
             }
-
+            // and we also add `adjacent` on an undefined plane on which `incident` lies
             intersections.entry(*incident).and_modify(|matchers| {
                 matchers.push((
                     PlaneMatcher::undefined(self.epsilon),
@@ -169,18 +204,23 @@ impl PathGraphBuilder {
                 ));
             });
         }
-
+        // constructs the path graph by keeping for each connection and for each detected plane on which it lies
+        // only the successor connection that minimizes the plane-projected clockwise angle
         PathGraph {
             intersections: intersections
                 .iter()
                 .map(|(source, matchers)| {
                     (
+                        // the current connection
                         *source,
+                        // all correspondigly detected planes
                         matchers
                             .iter()
                             .map(|(matcher, successors)| {
                                 (
+                                    // any detected plane
                                     *matcher,
+                                    // finds the successor connection that minimizes the projected clockwise angle
                                     successors
                                         .iter()
                                         .min_by(|u, v| u.angle.partial_cmp(&v.angle).unwrap())
@@ -192,6 +232,7 @@ impl PathGraphBuilder {
                     )
                 })
                 .collect(),
+            // threshold used to recognize the collinearity between a pair of connection
             epsilon: self.epsilon,
         }
     }
--- src/plane.rs
+++ src/plane.rs
@@ -1,45 +1,63 @@
 use super::coordinates::CoordinatesVector;
 
+/// Three-dimensional plane.
 #[derive(Clone, Copy, Debug)]
 struct Plane {
+    /// Normal vector of the plane.
     normal: CoordinatesVector,
+    /// Two basis vectors spanning the plane.
     basis: (CoordinatesVector, CoordinatesVector),
 }
 
+/// A plane matcher can represent a three-dimensional plane or an undefined one.
 #[derive(Clone, Copy, Debug)]
 pub struct PlaneMatcher {
+    /// Well-defined or undefined plane.
     plane: Option<Plane>,
+    /// Threshold controlling collinearity.
     epsilon: f64,
 }
 
 impl PlaneMatcher {
+    /// Constructs a plane if any on which both `current` and `successor` connections lie.
     pub fn between(
         current: &CoordinatesVector,
         successor: &CoordinatesVector,
         epsilon: f64,
     ) -> Self {
+        // first checks whether the two vectors have a defined cross product direction
         match current.cross(successor).normalize(epsilon) {
+            // if the cross product is not the zero vector then a plane exists
             Some(normal) => {
+                // computes the normal having a positive `z` entry
                 let normal = if normal.z < 0f64 {
                     normal.rescale(-1f64)
                 } else {
                     normal
                 };
-                let u = CoordinatesVector::random().normalize(f64::EPSILON).unwrap();
+                // computes the reference vector
+                let r = CoordinatesVector::random().normalize(f64::EPSILON).unwrap();
+                // computes the first vector of the basis
+                let u = normal.cross(&r).normalize(f64::EPSILON).unwrap();
+                // and the second
                 let v = normal.cross(&u).normalize(f64::EPSILON).unwrap();
-
+                // constructs plane matcher
                 PlaneMatcher {
+                    // plane
                     plane: Some(Plane {
                         normal,
                         basis: (u, v),
                     }),
+                    // collinearity threshold
                     epsilon,
                 }
             }
+            // if they have the zero vector as cross product then they are collinear and no plane is defined
             None => PlaneMatcher::undefined(epsilon),
         }
     }
 
+    /// Constructs an undefined plane using `epsilon` as collinearity threshold.
     pub fn undefined(epsilon: f64) -> Self {
         PlaneMatcher {
             plane: None,
@@ -47,10 +65,12 @@ impl PlaneMatcher {
         }
     }
 
+    /// Returns true if the plane has no defined direction.
     pub fn is_undefined(&self) -> bool {
         self.plane.is_none()
     }
 
+    /// Returns true if it shares the same plane with `other` meaning they have parallel normal vectors.
     pub fn is_same_as(&self, other: &Self) -> bool {
         match (self.plane, other.plane) {
             (Some(p), Some(q)) => p.normal.is_parallel_to(&q.normal, self.epsilon),
@@ -58,9 +78,12 @@ impl PlaneMatcher {
         }
     }
 
+    /// Returns a well-defined plane matcher only if it shares the same plane with `other`.
     pub fn match_against(&self, other: &Self) -> Option<PlaneMatcher> {
         match (self.plane, other.plane) {
+            // in case both planes are defined
             (Some(p), Some(q)) => {
+                // returns the current plane if both are parallel
                 if p.normal.is_parallel_to(&q.normal, self.epsilon) {
                     Some(*self)
                 } else {
@@ -73,6 +96,7 @@ impl PlaneMatcher {
         }
     }
 
+    /// Projects `vector` on the plane if defined as unit vector.
     pub fn project(&self, vector: &CoordinatesVector) -> Option<CoordinatesVector> {
         match self.plane {
             Some(plane) => CoordinatesVector {
@@ -85,13 +109,17 @@ impl PlaneMatcher {
         }
     }
 
+    /// Projects `current` and `successor` vectors on the plane and computes the clockwise
+    /// angle on the plane between the two.
     pub fn project_angle_between(
         &self,
         current: &CoordinatesVector,
         successor: &CoordinatesVector,
     ) -> Option<f64> {
+        // only if the vectors can be projected on the current plane
         match (self.project(current), self.project(successor)) {
             (Some(u), Some(v)) => {
+                // computes the clockwise angle projected on the plane
                 Some(std::f64::consts::PI + (v.y * u.x - v.x * u.y).atan2(u.x * v.x + u.y * v.y))
             }
             _ => None,
@@ -100,6 +128,7 @@ impl PlaneMatcher {
 }
 
 impl PartialEq for PlaneMatcher {
+    /// Same as [PlaneMatcher::is_same_as]
     fn eq(&self, other: &Self) -> bool {
         self.is_same_as(other)
     }
